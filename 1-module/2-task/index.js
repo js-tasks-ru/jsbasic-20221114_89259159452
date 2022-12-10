305658d769b97252463f8d830a26924e9ea0caf3
Ramazanov Bayram <89259159452@mail.ru>
2022-12-10 23:34:09 +0300

    Основы js

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -2,7 +2,7 @@
  * Эту функцию трогать не нужно
  */
 function print(text) {
-  console.log(text);
+    console.log(text);
 }
 
 /**
@@ -10,15 +10,18 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+    return !!name && !name.includes(' ') && name.length >= 4;
 }
 
-function sayHello() {
-  let userName = prompt('Введите ваше имя');
 
-  if (isValid(userName)) {
-    print(`Welcome back, ${userName}!`);
-  } else {
-    print('Некорректное имя');
-  }
+
+
+function sayHello() {
+    let userName = prompt('Введите ваше имя');
+
+    if (isValid(userName)) {
+        print(`Welcome back, ${userName}!`);
+    } else {
+        print('Некорректное имя');
+    }
 }
--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
-  // ваш код...
+    const ul = document.createElement('ul');
+
+    friends
+        .map(user => `<li>${user.firstName} ${user.lastName}</li>`)
+        .map(li => ul.innerHTML += li)
+    return ul
 }
