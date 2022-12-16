531887865a3d808afd48e193f95ac3dc799034a8
Ramazanov Bayram <89259159452@mail.ru>
2022-12-16 20:28:12 +0300

    дз 5 (2)

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
 function toggleText() {
-  // ваш код...
+    const toggle = document.querySelector('.toggle-text-button'),
+        text = document.querySelector('#text');
+    
+    let check = true;
+
+    toggle.addEventListener('click', ()=> {
+        (check) ? (check = false, text.hidden = true) : (check = true, text.hidden = false);
+    }) 
 }
