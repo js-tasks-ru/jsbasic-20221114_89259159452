bed808f1b69c88103938fc0a72fdcd953ba23e97
Ramazanov Bayram <89259159452@mail.ru>
2022-12-19 22:44:35 +0300

    Исправил с text.hidden

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,10 +1,8 @@
 function toggleText() {
     const toggle = document.querySelector('.toggle-text-button'),
         text = document.querySelector('#text');
-    
-    let check = true;
 
     toggle.addEventListener('click', ()=> {
-        (check) ? (check = false, text.hidden = true) : (check = true, text.hidden = false);
+        (text.hidden) ? (text.hidden = false) : (text.hidden = true);
     }) 
 }
